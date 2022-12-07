b8811218bfaf1be758f8c2fec5a2f5f67ecbd47f
lsoberanis <larasoberanis@tamu.edu>
2022-12-07 16:00:09 -0600

    updating some more

---
+++ javascript/slides.js
@@ -0,0 +1,11 @@
+(function($){
+$(document).ready(function(){
+    $('.slides-cont').slick({
+      //setting-name: setting-value
+      infinite: true,
+      dots: true,
+      speed: 300,
+      swipe: true,
+    });
+  });
+}) ( jQuery);
