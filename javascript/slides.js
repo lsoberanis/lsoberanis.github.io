b3d32feebc9f7d942ec2de2be0cab8d45208c917
lsoberanis <larasoberanis@tamu.edu>
2022-12-07 16:26:55 -0600

    navbar

--- javascript/slides.js
+++ javascript/slides.js
@@ -9,3 +9,24 @@ $(document).ready(function(){
     });
   });
 }) ( jQuery);
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
